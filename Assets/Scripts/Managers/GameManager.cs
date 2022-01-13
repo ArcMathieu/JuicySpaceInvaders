cc2c47087aaf29e137db9439b1579a48c81b02f7
Blue <mathieu.prevot@edu.devinci.fr>
2022-01-13 10:55:28 +0100

    waterChanges

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,23 +4,53 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     public Transform rightBorder;
     public Transform leftBorder;
     public Transform frontBorder;
     public Transform backBorder;
     public Transform gameOver;
     public Transform Score;
+    public CameraManager camManager;
+    public GameObject BarrageAnim;
+    public PlayerController PlayerPrefs;
+    public bool isStart;
+    void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        camManager = Gino.instance.cameraManager;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            LaunchGame();
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Space))
-    //        LaunchGameOver();
-    //}
+    public void LaunchGame()
+    {
+        camManager.cam1.gameObject.GetComponent<Animator>().SetTrigger("Start");
+        BarrageAnim.GetComponent<Animator>().SetTrigger("Start");
+        StartCoroutine(waitToSpawnPlayer());
+    }
 
-    //public void LaunchGameOver()
-    //{
-    //    gameOver.GetComponent<Animator>().SetBool("GO", true);
-    //    Score.GetComponent<Animator>().SetTrigger("GameOver");
-    //}
+    IEnumerator waitToSpawnPlayer()
+    {
+        yield return new WaitForSeconds(4);
+        Instantiate(PlayerPrefs, Vector3.zero, Quaternion.identity, BarrageAnim.transform);
+    }
+    
+    public void LaunchGameOver()
+    {
+        //arreter enemies + decor
+        gameOver.GetComponent<Animator>().SetBool("GO", true);
+        Score.GetComponent<Animator>().SetTrigger("GameOver");
+    }
+    public void RestartGame()
+    {
+        
+    }
 }
