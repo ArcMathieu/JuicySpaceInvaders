486c3ddc12d3b5e6512850566cfbadf876b60605
Blue <mathieu.prevot@edu.devinci.fr>
2022-01-14 10:19:23 +0100

    uima

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -6,15 +6,10 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public GameObject uiJuicyParent;
-    public Image gameOver;
     public int score = 0;
     //public CameraController camera;
     public Image[] lifes = new Image[0];
     public int totalKilled = 0;
-
-    public void GameOver() {
-        gameOver.gameObject.SetActive(true);
-    }
 
     public void AddScore(int addScore) {
         score += addScore;
